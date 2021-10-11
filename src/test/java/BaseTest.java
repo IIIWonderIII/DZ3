70dcd0285f834cd0e5992757c2e55d5670c1d85d
IIIwonderIII <mr.rybalkoo@mail.ru>
2021-10-11 17:43:32 +0300

    DZ3

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import io.restassured.RestAssured;
+import java.io.*;
+import java.util.Properties;
+import org.junit.jupiter.api.BeforeAll;
+import io.qameta.allure.restassured.*;
+
+
+public abstract class BaseTest {
+    static Properties properties = new Properties();
+    static String token;
+    static String username;
+
+    @BeforeAll
+    static void beforeAll() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.filters(new AllureRestAssured());
+        getProperties();
+        token = properties.getProperty("token");
+        username = properties.getProperty("username");
+    }
+    private static void getProperties() {
+        try (InputStream output = new FileInputStream("src/test/resources/application.properties")) {
+            properties.load(output);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
